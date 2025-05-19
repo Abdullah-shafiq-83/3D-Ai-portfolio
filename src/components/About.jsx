de8ae442038e23599aefdc848cb147321fa1d292
abdullah-shafiq-83 <abdullahshafiq2429@gmail.com>
2025-05-19 19:45:21 +0500

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,28 +8,25 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    className="xs:w-[250px] w-full"
+    tiltMaxAngleX={45}
+    tiltMaxAngleY={45}
+    scale={1}
+    transitionSpeed={450}
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt={`${title} icon`}
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
+        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
       </div>
     </motion.div>
   </Tilt>
@@ -45,19 +42,21 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        As a dedicated AI Engineer and Flutter Developer, I specialize in designing and implementing intelligent systems
-        that drive innovation.With a strong focus on machine learning models and AI-driven solutions,
-        I work to optimize performance, automate processes, and create adaptive technologies.
-
-        Additionally, I build scalable and visually appealing mobile applications using Flutter,
-        delivering seamless cross-platform experiences. Combining expertise in both AI and Flutter, 
-        I deliver cutting-edge solutions that address real-world challenges, enhance user experience, 
-        and drive business success.
+        As a dedicated AI Engineer and Flutter Developer, I specialize in designing and
+        implementing intelligent systems that drive innovation. With a strong focus on
+        machine learning models and AI-driven solutions, I work to optimize performance,
+        automate processes, and create adaptive technologies.
+        <br />
+        <br />
+        Additionally, I build scalable and visually appealing mobile applications using
+        Flutter, delivering seamless cross-platform experiences. Combining expertise in both
+        AI and Flutter, I deliver cutting-edge solutions that address real-world challenges,
+        enhance user experience, and drive business success.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
